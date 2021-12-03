f5044c8a871e7d2944ea431721095740fd6dea48
IFRaseck <leonhurtado22@gmail.com>
2021-12-03 13:08:13 -0500

    123

--- models/pedido.js
+++ models/pedido.js
@@ -2,31 +2,31 @@ var mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const pedidoSchema = new Schema({
-    producto: {
-        type: Array,
-        tamaño: String, 
-        unidades: String, 
-        complementos: String,
-        },
-    comprador: {
-        type: Array,
-        nombres: String,
-        apellidos: String,
-        tipoDocumento: String,
-        numDocumento: String,
-        direccion: String,
-        numeroCasa: String,
-        barrio: String,
-        telefono: String,
-        fechaEntrega: String,
-        }, 
+    // producto: {
+    //     type: Array,
+    //     tamaño: String, 
+    //     unidades: String, 
+    //     complementos: String,
+    //     },
+    // comprador: {
+    //     type: Array,
+    //     nombres: String,
+    //     apellidos: String,
+    //     tipoDocumento: String,
+    //     numDocumento: String,
+    //     direccion: String,
+    //     numeroCasa: String,
+    //     barrio: String,
+    //     telefono: String,
+    //     fechaEntrega: String,
+    //     }, 
     
-    // nombre: {type: String, required:[true, 'Nombre Obligatorio']},
-    // telefono: String,
-    // tipo: String,
-    // cantidad: String,
-    // date: {type: Date, default:Date.now},
-    // active: {type: Boolean, default:true},
+    nombre: {type: String, required:[true, 'Nombre Obligatorio']},
+    telefono: String,
+    tipo: String,
+    cantidad: String,
+    date: {type: Date, default:Date.now},
+    active: {type: Boolean, default:true},
 
 
 });
